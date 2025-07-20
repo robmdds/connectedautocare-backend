52e65cdf111790b7e8da5ba54e16fd532b88aa87
robmdds <robm@dynamicdealerservices.com>
2025-07-20 10:19:22 +0300

    fix for vercel

--- api/index.py
+++ api/index.py
@@ -1167,3 +1167,5 @@ def handler(request):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+else:
+    application = app  # For WSGI servers like Gunicorn or uWSGI
